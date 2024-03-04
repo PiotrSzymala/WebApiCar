0c60502c4431358005ce4b96b34d5c38c98c6b28
Piotr Szymala <p.szymala@sellpander.pl>
2024-03-04 22:23:18 +0100

    Add authorization.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using FluentValidation;
+using WebApiCar.Application.Services;
+using WebApiCar.Infrastructure.ConnectionStrings;
 using WebApiCar.Infrastructure.DatabseContexts;
+using WebApiCar.Infrastructure.Middlewares;
+using WebApiCar.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace WebApiCar
 {
@@ -18,10 +26,53 @@ namespace WebApiCar
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var connectionString = builder.Configuration.GetConnectionString("test");
+            builder.Services.Configure<PostgresSqlConnectionString>(
+                builder.Configuration.GetSection(nameof(PostgresSqlConnectionString)));
 
-            builder.Services.AddDbContext<CarDbContext>(options => options.UseNpgsql(connectionString));
-                
+            builder.Services.AddTransient<PostgresSqlConnectionString>();
+            builder.Services.AddDbContext<CarDbContext>(
+                (sp, o) =>
+                {
+                    var connectionString =
+                        sp.GetRequiredService<PostgresSqlConnectionString>().Value;
+                    o.UseNpgsql(connectionString);
+                }
+            );
+
+            builder.Services.AddScoped<ExceptionMiddleware>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<ICarService, CarService>();
+            builder.Services.AddScoped<ICarRepository, CarRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.Name = "carApi.Authentication";
+                options.ExpireTimeSpan = TimeSpan.FromHours(8);
+                options.Cookie.IsEssential = true;
+                options.SlidingExpiration = true;
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+                options.Events.OnRedirectToAccessDenied = context =>
+                {
+                    context.Response.StatusCode = 403;
+                    return Task.CompletedTask;
+                };
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -33,8 +84,8 @@ namespace WebApiCar
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
