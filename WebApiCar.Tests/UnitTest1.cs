4fda969776c7ea120b56cbd339be5f844e2f41e0
Piotr Szymala <p.szymala@sellpander.pl>
2024-03-27 17:56:55 +0100

    Add tests.

---
+++ WebApiCar.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace WebApiCar.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //test
+            var test = new UnitTest1();
+        }
+    }
+}
